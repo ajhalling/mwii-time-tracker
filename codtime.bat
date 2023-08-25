f8c8391b22b1f14f4e7ccf77dbad8bf8fe830dff
AJ Halling <aj.halling7@gmail.com>
2023-08-24 22:53:28 -0600

    Update: Thu 08/24/2023 from HTPC

--- codtime.bat
+++ codtime.bat
@@ -10,7 +10,7 @@ REM Add all changes (including the updated local file)
 git add -A
 
 REM Commit the changes with a message
-git commit -m "Update: $(date) from HTPC"
+git commit -m "Update: %date% from HTPC"
 
-REM Push the changes to the remote repository using the PAT from the environment variable
+REM Push the changes to the remote repository using the PAT from environment variable
 git push origin master -c http.extraheader="Authorization: Bearer %GITHUB_PAT%"
