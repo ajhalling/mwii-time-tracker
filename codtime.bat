5897214efd1c45cbfac4aff5f1c62d5e588ce61e
AJ Halling <aj.halling7@gmail.com>
2023-08-24 23:00:35 -0600

    Update: Thu 08/24/2023 from HTPC

--- codtime.bat
+++ codtime.bat
@@ -1,12 +1,14 @@
 @echo off
 
-REM change directory to repo
+REM Change directory to repo
 cd /d "C:\Code\ajhalling\mwii-time-tracker"
 
-REM run refresh_data script to update .csv
-python dash\scripts\refresh_data.py
+REM Run refresh_data script to update .csv
+cd dash\data
+python ..\..\dash\scripts\refresh_data.py
+cd ..
 
-REM Add all changes (including the updated local file)
+REM Add all changes (including the updated .csv file)
 git add -A
 
 REM Commit the changes with a message
