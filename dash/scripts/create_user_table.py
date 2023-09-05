e0ad4a8fdabdf41589dab4a7b1758df1cd6572be
AJ <aj.halling7@gmail.com>
2023-09-04 21:39:47 -0600

    update

--- dash/scripts/create_user_table.py
+++ dash/scripts/create_user_table.py
@@ -5,18 +5,18 @@ db_file = "dash\data\gametime.db"
 conn = sqlite3.connect(db_file)
 cur = conn.cursor()
 
-# Create a User Profile table
+# # Create a User Profile table
 # cur.execute('''CREATE TABLE IF NOT EXISTS users
 #                 (steamid INTEGER , name TEXT)''')
 
-# Test data
+# # Test data
 # user_list = [
 #     (76561198044737410, 'Dickson'), 
 #     (76561198037729598, 'AJ'), 
 #     (76561198102561840, 'Ashton')
 # ]
 
-# Insert data into database
+# # Insert data into database
 # cur.executemany('''
 #         INSERT INTO users (steamid, name) VALUES (?, ?)
 # ''', user_list)
